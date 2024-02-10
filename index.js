020ddb492d17a14713b7a72b44860b3c75f8b1c5
sonDang612 <sonhoangdang0612@gmail.com>
2024-02-11 06:15:48 +0700

    feat: test connection

--- index.js
+++ index.js
@@ -1,24 +1,13 @@
-import cors from "cors";
-import express from "express";
-import connectDB from "./src/config/connectDB";
-import initRoutes from "./routes";
-require("dotenv").config();
+const express = require("express");
+
 const app = express();
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    methods: ["POST", "GET", "PUT", "DELETE"],
-  })
-);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
-initRoutes(app);
-connectDB();
-
-const port = process.env.PORT || 8888;
-const listener = app.listen(port, () => {
-  console.log(`Server is running on the port ${listener.address().port}....`);
+app.get("/", (req, res) => {
+  res.json({ msg: "on" });
 });
 
-export default app;
+const listen = app.listen(5000, () => {
+  console.log("on");
+});
+
+module.exports = app;
